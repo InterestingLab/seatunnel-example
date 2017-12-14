de6ba3ec827986a49dfbfa63948ce4f3b7293e8c
RickyHuo <huochen1994@163.com>
2017-12-14 10:14:06 +0800

    init

---
+++ src/main/java/org/interestinglab/waterdrop_test/filter/Javasubstring.java
@@ -0,0 +1,56 @@
+package org.interestinglab.waterdrop_test.filter;
+
+import com.typesafe.config.ConfigFactory;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.substring;
+import org.interestinglab.waterdrop.apis.BaseFilter;
+
+import com.typesafe.config.Config;
+import org.apache.spark.streaming.StreamingContext;
+import scala.Tuple2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Javasubstring extends BaseFilter {
+
+    private Config config;
+
+    public Javasubstring(Config config) {
+        super (config);
+    }
+
+    @Override
+    public Tuple2<Object, String> checkConfig() {
+        if(!config.hasPath("len")) {
+            return new Tuple2<>(false, "please specify [len]");
+        }
+        System.out.println("works");
+        System.out.println(new Tuple2<>(true, ""));
+        return new Tuple2<>(true, "");
+    }
+
+    @Override
+    public void prepare(SparkSession spark, StreamingContext ssc) {
+
+        Map<String, Object> map = new HashMap();
+        map.put("source_field", "raw_message");
+        map.put("target_field", "__ROOT__");
+        map.put("pos", 0);
+        Config defaultConfig = ConfigFactory.parseMap(map);
+
+        config = config.withFallback(defaultConfig);
+    }
+
+    @Override
+    public Dataset<Row> process(SparkSession spark, Dataset<Row> df) {
+        String srcField = config.getString("source_field");
+        String targetField = config.getString("target_field");
+        int pos = config.getInt("fields");
+        int len = config.getInt("fields");
+        return df.withColumn(targetField, substring(col(srcField), pos, len));
+    }
+}
---
+++ src/main/scala/org/interestinglab/waterdrop_test/filter/Scalasubstring.scala
@@ -0,0 +1,56 @@
+package org.interestinglab.waterdrop_test.filter
+
+import org.interestinglab.waterdrop.apis.BaseFilter
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{substring, col}
+import org.apache.spark.streaming.StreamingContext
+
+import scala.collection.JavaConversions._
+
+class Scalasubstring(var config: Config) extends BaseFilter(config) {
+
+  def this() {
+    this(ConfigFactory.empty())
+  }
+
+  override def checkConfig(): (Boolean, String) = {
+
+    val requiredOptions = List("source_field", "len")
+    val nonExistsOptions:List[(String, Boolean)] = requiredOptions.map{ optionName =>
+      (optionName, config.hasPath(optionName))
+    }.filter { p =>
+      val (optionName, exists) = p
+        !exists
+      }
+
+    if (nonExistsOptions.length == 0) {
+      (true, "")
+    } else {
+      (false, "please specify source_field as non-empty string")
+    }
+  }
+
+  override def prepare(spark: SparkSession, ssc: StreamingContext): Unit = {
+
+    val defaultConfig = ConfigFactory.parseMap(
+      Map(
+        "source_field" -> "raw_message",
+        "target_field" -> "__ROOT__",
+        "pos" -> 0
+      )
+    )
+
+    config = config.withFallback(defaultConfig)
+  }
+
+  override def process(spark: SparkSession, df: DataFrame): DataFrame = {
+
+    val srcField = config.getString("source_field")
+    val targetField = config.getString("target_field")
+    val pos = config.getInt("fields")
+    val len = config.getInt("fields")
+    df.withColumn(targetField, substring(col(srcField), pos, len))
+  }
+
+}
